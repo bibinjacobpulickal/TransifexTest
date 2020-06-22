541e5063d035c9c55070ef033100d1d5ed8cb6b2
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2020-06-22 11:55:04 +0530

    Initial Commit

---
+++ TransifexTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransifexTest
+//
+//  Created by Bibin Jacob Pulickal on 22/06/20.
+//  Copyright © 2020 bibinjacobpulickal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
